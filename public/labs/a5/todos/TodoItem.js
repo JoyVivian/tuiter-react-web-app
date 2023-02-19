2141709ae4ff405f23e6a43b3f3f96ff8922f667
Wei Zhang <zhang.wei8@northeastern.edu>
2023-02-19 16:12:33 -0500

    Finish Labs.

---
+++ public/labs/a5/todos/TodoItem.js
@@ -0,0 +1,13 @@
+const TodoItem = (todo) => {
+    return (`
+         <input type="checkbox"
+        ${todo.done ? 'checked' : ''}/>
+
+       <li>
+            ${todo.title}
+           (${todo.status})
+        </li>
+   `);
+}
+export default TodoItem;
+
