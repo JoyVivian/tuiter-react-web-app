6dbaa66aa29ea71703e3d0f8880fefe77d43996b
Wei Zhang <zhang.wei8@northeastern.edu>
2023-03-26 17:39:22 -0400

    Update todo

--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, {useState} from "react";
+import {useSelector} from "react-redux";
 
 const Todos = () => {
     const todos = useSelector(state => state.todos);
 
+    const [todo, setTodo] = useState({do: ''});
+    const todoChangeHandler = (event) => {
+        const doValue = event.target.value;
+        const newTodo = {
+            do: doValue
+        };
+        setTodo(newTodo);
+    }
+
     return (
         <>
-        <h3>Todos</h3>
+            <h3>Todos</h3>
             <ul className="list-group">
+                <li className="list-group-item">
+                    <input onChange={todoChangeHandler} value={todo.do} className="form-control"/>
+                </li>
                 {
                     todos.map(todo => <li className="list-group-item">
-                        {todo.do}
-                    </li>
+                            {todo.do}
+                        </li>
                     )
                 }
             </ul>
