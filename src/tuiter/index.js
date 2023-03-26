f3475edba707273fa6de301cd0accd96b1f7a12f
Wei Zhang <zhang.wei8@northeastern.edu>
2023-03-26 12:51:00 -0400

    refactor who-to-follow-list.

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,25 +4,31 @@ import WhoToFollowList from './who-to-follow-list';
 import ExploreComponent from './explore';
 import {Routes, Route} from 'react-router';
 import HomeScreen from './home';
+import whoReducer from "./reducers/who-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const store = configureStore({reducer: {who: whoReducer}});
 
 function Tuiter() {
   return (
+      <Provider store={store}>
     <div className="row mt-2">
-      {/* uncomment these lines to test whotofollow list.*/}
-      {/* <div className="col-2 col-md-2 col-lg-1 col-xl-2">*/}
-      {/*    <NavigationSidebar active="explore"/>*/}
-      {/* </div>*/}
-      {/* <div className="col-10 col-md-10 col-lg-7 col-xl-6"*/}
-      {/*     style={{"position": "relative"}}>*/}
-      {/*    <ExploreComponent/>*/}
-      {/* </div>*/}
-      {/* <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">*/}
-      {/*    <WhoToFollowList/>*/}
-      {/* </div>*/}
+       {/*uncomment these lines to test whotofollow list.*/}
+       <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+          <NavigationSidebar active="explore"/>
+       </div>
+       <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+           style={{"position": "relative"}}>
+          <ExploreComponent/>
+       </div>
+       <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+          <WhoToFollowList/>
+       </div>
 
-      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-        <NavigationSidebar active="explore"/>
-      </div>
+      {/*<div className="col-2 col-md-2 col-lg-1 col-xl-2">*/}
+      {/*  <NavigationSidebar active="explore"/>*/}
+      {/*</div>*/}
 
       <div className="col-10 col-md-10 col-lg-7 col-xl-6" style={{'position': 'relative'}}>
         <Routes>
@@ -31,7 +37,7 @@ function Tuiter() {
         </Routes>
       </div>
     </div>
-
+      </Provider>
   );
 }
 
